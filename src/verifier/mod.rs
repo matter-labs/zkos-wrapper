3f122e4dfa9cea9c1cf3cfc368139bf570b1a1ec
olesHolem <oh@matterlabs.dev>
2024-12-19 17:01:05 +0200

    added dependencies and some allocateble structs

---
+++ src/verifier/mod.rs
@@ -0,0 +1,38 @@
+use boojum::field::SmallField;
+use boojum::gadgets::u32::UInt32;
+use boojum::cs::traits::cs::ConstraintSystem;
+use boojum::gadgets::mersenne_field::fourth_ext::MersenneQuartic;
+use boojum::gadgets::mersenne_field::MersenneField;
+use boojum::gadgets::traits::allocatable::CSAllocatable;
+
+use zkos_verifier::concrete::size_constants::*;
+use zkos_verifier::prover::definitions::*;
+use zkos_verifier::prover::cs::definitions::{
+    NUM_DELEGATION_ARGUMENT_LINEARIZATION_CHALLENGES, REGISTER_SIZE,
+    NUM_MEM_ARGUMENT_LINEARIZATION_CHALLENGES
+};
+use zkos_verifier::field::*;
+use zkos_verifier::verifier_common::{ProofPublicInputs, ProofOutput};
+
+mod prover_structs;
+
+use prover_structs::*;
+
+pub unsafe fn verify<
+    F: SmallField,
+    CS: ConstraintSystem<F>,
+    // I: NonDeterminismSource,
+    // V: LeafInclusionVerifier
+>(
+    proof_state_dst: &mut WrappedProofOutput<
+        F, 
+        TREE_CAP_SIZE,
+        NUM_COSETS,
+        NUM_DELEGATION_CHALLENGES,
+        NUM_AUX_BOUNDARY_VALUES,
+    >,
+    proof_input_dst: &mut WrappedProofPublicInputs<F, NUM_STATE_ELEMENTS>,
+) {
+    todo!()
+}
+
