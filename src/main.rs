00ae157a9f3971c80065d8fd9b8341b529c58ab1
mm-zk <mm@matterlabs.dev>
2025-04-08 07:58:30 +0200

    updated CLI

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use clap::Parser;
 
 use zkos_wrapper::{
-    get_zkos_wrapper_proof, get_zkos_wrapper_setup, verify_zkos_wrapper_proof,
+    get_risc_wrapper_setup, prove_risc_wrapper, verify_risc_wrapper_proof,
     zkos_utils::read_and_verify_proof,
 };
 
@@ -20,28 +20,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
     let worker = boojum::worker::Worker::new_with_num_threads(4);
-    let (proof, _, _) = read_and_verify_proof(&cli.input);
+    let risc_wrapper_witness = read_and_verify_proof(&cli.input);
 
-    let (finalization_hint, setup_base, setup, vk, setup_tree, vars_hint, witness_hints) =
-        get_zkos_wrapper_setup(&worker);
-    let proof = get_zkos_wrapper_proof(
-        proof,
+    let (
+        finalization_hint,
+        setup_base,
+        setup,
+        risc_wrapper_vk,
+        setup_tree,
+        vars_hint,
+        witness_hints,
+    ) = get_risc_wrapper_setup(&worker);
+
+    let risc_wrapper_proof = prove_risc_wrapper(
+        risc_wrapper_witness,
         &finalization_hint,
         &setup_base,
         &setup,
-        &vk,
+        &risc_wrapper_vk,
         &setup_tree,
         &vars_hint,
         &witness_hints,
         &worker,
     );
-
-    let is_valid = verify_zkos_wrapper_proof(&proof, &vk);
+    let is_valid = verify_risc_wrapper_proof(&risc_wrapper_proof, &risc_wrapper_vk);
 
     assert!(is_valid);
 
     let proof_file = File::create(cli.output)?;
-    serde_json::to_writer(proof_file, &proof)?;
+    serde_json::to_writer(proof_file, &risc_wrapper_proof)?;
 
     Ok(())
 }
--- src/zkos_utils.rs
+++ src/zkos_utils.rs
@@ -1,26 +1,22 @@
-use crate::verify_zkos_proof;
-use zkos_verifier::prover::prover_stages::Proof;
+use risc_verifier::prover::prover_stages::Proof;
 
-use zkos_verifier::concrete::size_constants::*;
-use zkos_verifier::prover::definitions::Blake2sForEverythingVerifier;
-use zkos_verifier::verifier_common::{ProofOutput, ProofPublicInputs};
+use crate::circuits::RiscWrapperWitness;
 
-pub fn read_and_verify_proof(
-    proof_path: &String,
-) -> (
-    Proof,
-    ProofOutput<TREE_CAP_SIZE, NUM_COSETS, NUM_DELEGATION_CHALLENGES, NUM_AUX_BOUNDARY_VALUES>,
-    ProofPublicInputs<NUM_STATE_ELEMENTS>,
-) {
+pub fn read_and_verify_proof(proof_path: &String) -> RiscWrapperWitness {
     // read proof from file
     println!("Verifying proof from {}", proof_path);
     let proof: Proof = deserialize_from_file(proof_path);
 
-    // verify proof
-    let (proof_state_dst, proof_input_dst) =
-        verify_zkos_proof::<Blake2sForEverythingVerifier>(&proof);
+    // TODO: read from somewhere.
+    let final_registers_state = vec![0u32; 64];
 
-    (proof, proof_state_dst, proof_input_dst)
+    let risc_wrapper_witness = RiscWrapperWitness {
+        final_registers_state: final_registers_state.try_into().unwrap(),
+        proof,
+    };
+    // TODO: check if proof is valid.
+
+    risc_wrapper_witness
 }
 
 fn deserialize_from_file<T: serde::de::DeserializeOwned>(filename: &str) -> T {
