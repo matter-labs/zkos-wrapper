10641aab331fe17e2d3406fd700b2fa0bc15d835
mm <mm@matterlabs.dev>
2025-05-16 11:05:00 +0200

    allow using proper CRS and generate verification keys

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::alloc::Global;
 // - wrapping the proof into SNARK.
 use std::path::Path;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 use bellman::kate_commitment::{Crs, CrsForMonomialForm};
 use bellman::worker::Worker as BellmanWorker;
@@ -20,7 +20,7 @@ use execution_utils::{
 };
 use risc_verifier::prover::worker::Worker;
 use zkos_wrapper::circuits::BinaryCommitment;
-use zkos_wrapper::{Bn256, L1_VERIFIER_DOMAIN_SIZE_LOG};
+use zkos_wrapper::{Bn256, L1_VERIFIER_DOMAIN_SIZE_LOG, calculate_verification_key_hash};
 use zkos_wrapper::{
     circuits::RiscWrapperWitness, get_compression_setup, get_risc_wrapper_setup,
     get_snark_wrapper_setup, prove_compression, prove_risc_wrapper, prove_snark_wrapper,
@@ -30,16 +30,45 @@ use zkos_wrapper::{
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    input: String,
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    // Binary used to generate the proof.
-    // If not specified, take the default binary (fibonacci hasher).
-    #[arg(long)]
-    input_binary: Option<String>,
+#[derive(Subcommand)]
+enum Commands {
+    /// Take the riscV final proof, and create a SNARK proof.
+    Prove {
+        #[arg(short, long)]
+        input: String,
 
-    #[arg(short, long)]
-    output_dir: String,
+        // Binary used to generate the proof.
+        // If not specified, take the default binary (fibonacci hasher).
+        #[arg(long)]
+        input_binary: Option<String>,
+
+        #[arg(short, long)]
+        output_dir: String,
+
+        /// File with the trusted setup.
+        /// If missing - will use the 'fake' trusted setup.
+        #[arg(long)]
+        trusted_setup_file: Option<String>,
+    },
+    /// Generate verification key for the SNARK proof.
+    GenerateVk {
+        // Binary used to generate the proof.
+        // If not specified, take the default binary (fibonacci hasher).
+        #[arg(long)]
+        input_binary: String,
+
+        #[arg(short, long)]
+        output_dir: String,
+
+        /// File with the trusted setup.
+        /// If missing - will use the 'fake' trusted setup.
+        #[arg(long)]
+        trusted_setup_file: String,
+    },
 }
 
 fn serialize_to_file<T: serde::ser::Serialize>(content: &T, filename: &Path) {
@@ -50,6 +79,14 @@ fn serialize_to_file<T: serde::ser::Serialize>(content: &T, filename: &Path) {
 fn deserialize_from_file<T: serde::de::DeserializeOwned>(filename: &str) -> T {
     let src = std::fs::File::open(filename).unwrap();
     serde_json::from_reader(src).unwrap()
+}
+
+/// Uploads trusted setup file to the RAM
+pub fn get_trusted_setup(crs_file_str: &String) -> Crs<Bn256, CrsForMonomialForm> {
+    let crs_file_path = std::path::Path::new(crs_file_str);
+    let crs_file = std::fs::File::open(&crs_file_path)
+        .expect(format!("Trying to open CRS FILE: {:?}", crs_file_path).as_str());
+    Crs::read(&crs_file).expect(format!("Trying to read CRS FILE: {:?}", crs_file_path).as_str())
 }
 
 pub fn create_binary_commitment(
@@ -103,12 +140,93 @@ pub fn create_binary_commitment(
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
+    match cli.command {
+        Commands::Prove {
+            input,
+            input_binary,
+            output_dir,
+            trusted_setup_file,
+        } => {
+            println!("=== Phase 0: Proving");
+            prove(input, input_binary, output_dir, trusted_setup_file)?;
+        }
+        Commands::GenerateVk {
+            input_binary,
+            output_dir,
+            trusted_setup_file,
+        } => {
+            println!("=== Phase 0: Generating the verification key");
+            generate_vk(input_binary, output_dir, trusted_setup_file)?;
+        }
+    }
+    Ok(())
+}
+
+fn generate_vk(
+    input_binary: String,
+    output_dir: String,
+    trusted_setup_file: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let worker = BellmanWorker::new_with_cpus(4);
+    let boojum_worker = boojum::worker::Worker::new_with_num_threads(4);
+
+    // FIXME.
+    let trusted_setup_file = None;
+
+    let crs_mons = match trusted_setup_file {
+        Some(ref crs_file_str) => get_trusted_setup(crs_file_str),
+        None => Crs::<Bn256, CrsForMonomialForm>::crs_42(
+            1 << L1_VERIFIER_DOMAIN_SIZE_LOG,
+            &BellmanWorker::new(),
+        ),
+    };
+    // FIXME: add universal vs non-universal.
+    let binary_commitment = create_binary_commitment(
+        input_binary,
+        &universal_circuit_no_delegation_verifier_vk().params,
+    );
+
+    let (_, _, _, risc_wrapper_vk, _, _, _) =
+        get_risc_wrapper_setup(&boojum_worker, binary_commitment.clone());
+
+    let (_, _, _, compression_vk, _, _, _) =
+        get_compression_setup(risc_wrapper_vk.clone(), &boojum_worker);
+
+    let (_, snark_wrapper_vk) = get_snark_wrapper_setup(compression_vk.clone(), &crs_mons, &worker);
+
+    serialize_to_file(
+        &snark_wrapper_vk,
+        &Path::new(&output_dir.clone()).join("snark_vk_expected.json"),
+    );
+
+    println!(
+        "VK key hash: {:?}",
+        calculate_verification_key_hash(snark_wrapper_vk)
+    );
+
+    Ok(())
+}
+
+fn prove(
+    input: String,
+    input_binary: Option<String>,
+    output_dir: String,
+    trusted_setup_file: Option<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let crs_mons = match trusted_setup_file {
+        Some(ref crs_file_str) => get_trusted_setup(crs_file_str),
+        None => Crs::<Bn256, CrsForMonomialForm>::crs_42(
+            1 << L1_VERIFIER_DOMAIN_SIZE_LOG,
+            &BellmanWorker::new(),
+        ),
+    };
+
     println!("=== Phase 1: Creating the Risc wrapper proof");
 
     let worker = boojum::worker::Worker::new_with_num_threads(4);
 
-    let program_proof: zkos_wrapper::ProgramProof = deserialize_from_file(&cli.input);
-    let binary_commitment = match cli.input_binary {
+    let program_proof: zkos_wrapper::ProgramProof = deserialize_from_file(&input);
+    let binary_commitment = match input_binary {
         Some(binary_path) => create_binary_commitment(binary_path, &program_proof.end_params),
         None => BinaryCommitment::from_default_binary(),
     };
@@ -143,7 +261,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     serialize_to_file(
         &risc_wrapper_proof,
-        &Path::new(&cli.output_dir.clone()).join("risc_proof.json"),
+        &Path::new(&output_dir.clone()).join("risc_proof.json"),
     );
 
     println!("=== Phase 2: Creating compression proof");
@@ -175,16 +293,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     serialize_to_file(
         &compression_proof,
-        &Path::new(&cli.output_dir.clone()).join("compression_proof.json"),
+        &Path::new(&output_dir.clone()).join("compression_proof.json"),
     );
 
     println!("=== Phase 3: Creating SNARK proof");
 
     {
         let worker = BellmanWorker::new_with_cpus(4);
-
-        let crs_mons =
-            Crs::<Bn256, CrsForMonomialForm>::crs_42(1 << L1_VERIFIER_DOMAIN_SIZE_LOG, &worker);
 
         let (snark_setup, snark_wrapper_vk) =
             get_snark_wrapper_setup(compression_vk.clone(), &crs_mons, &worker);
@@ -203,11 +318,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         serialize_to_file(
             &snark_wrapper_proof,
-            &Path::new(&cli.output_dir.clone()).join("snark_proof.json"),
+            &Path::new(&output_dir.clone()).join("snark_proof.json"),
         );
         serialize_to_file(
             &snark_wrapper_vk,
-            &Path::new(&cli.output_dir.clone()).join("snark_vk.json"),
+            &Path::new(&output_dir.clone()).join("snark_vk.json"),
         );
     }
 
