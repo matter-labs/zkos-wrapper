cdff533b28d2da4b2cfa2b071df030ee99e33c7f
EmilLuta <virgil.luta@gmail.com>
2025-05-31 00:41:13 +0200

    Update to work with airbender
    This PR updates the entire codebase to work with airbender (instead of
    old air_compiler). Additionally, makes `prove` function publicly
    available (to be later used in snark proving for zksync-os). The split
    to public is dirty.

--- src/main.rs
+++ src/main.rs
@@ -76,71 +76,11 @@ enum Commands {
     },
 }
 
-fn serialize_to_file<T: serde::ser::Serialize>(content: &T, filename: &Path) {
-    let src = std::fs::File::create(filename).unwrap();
-    serde_json::to_writer_pretty(src, content).unwrap();
-}
 
-fn deserialize_from_file<T: serde::de::DeserializeOwned>(filename: &str) -> T {
-    let src = std::fs::File::open(filename).unwrap();
-    serde_json::from_reader(src).unwrap()
-}
 
-/// Uploads trusted setup file to the RAM
-pub fn get_trusted_setup(crs_file_str: &String) -> Crs<Bn256, CrsForMonomialForm> {
-    let crs_file_path = std::path::Path::new(crs_file_str);
-    let crs_file = std::fs::File::open(&crs_file_path)
-        .expect(format!("Trying to open CRS FILE: {:?}", crs_file_path).as_str());
-    Crs::read(&crs_file).expect(format!("Trying to read CRS FILE: {:?}", crs_file_path).as_str())
-}
 
-pub fn create_binary_commitment(
-    binary_path: String,
-    expected_end_params: &[u32; 8],
-) -> BinaryCommitment {
-    let bin = std::fs::read(binary_path).unwrap();
 
-    let worker = Worker::new_with_num_threads(8);
 
-    let expected_final_pc = execution_utils::find_binary_exit_point(&bin);
-    let binary: Vec<u32> = execution_utils::get_padded_binary(&bin);
-
-    let base_params = execution_utils::compute_end_parameters(
-        expected_final_pc,
-        &setups::get_main_riscv_circuit_setup::<Global>(&binary, &worker),
-    );
-
-    // Check which verifier was used.
-    if universal_circuit_no_delegation_verifier_vk().params == *expected_end_params {
-        let layers = vec![
-            [0u32; 8],
-            base_params,
-            universal_circuit_verifier_vk().params,
-            universal_circuit_no_delegation_verifier_vk().params,
-        ];
-        BinaryCommitment {
-            end_params: universal_circuit_no_delegation_verifier_vk().params,
-            aux_params: execution_utils::compute_chain_encoding(layers),
-        }
-    } else if final_recursion_layer_verifier_vk().params == *expected_end_params {
-        let layers = vec![
-            [0u32; 8],
-            base_params,
-            recursion_layer_verifier_vk().params,
-            recursion_layer_no_delegation_verifier_vk().params,
-            final_recursion_layer_verifier_vk().params,
-        ];
-        BinaryCommitment {
-            end_params: final_recursion_layer_verifier_vk().params,
-            aux_params: execution_utils::compute_chain_encoding(layers),
-        }
-    } else {
-        panic!(
-            "Cannot find a verifier for the proof end parameters: {:?}",
-            expected_end_params
-        );
-    }
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
@@ -153,7 +93,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             trusted_setup_file,
         } => {
             println!("=== Phase 0: Proving");
-            prove(input, input_binary, output_dir, trusted_setup_file)?;
+            zkos_wrapper::prove(input, input_binary, output_dir, trusted_setup_file)?;
         }
         Commands::GenerateVk {
             input_binary,
@@ -185,7 +125,7 @@ fn generate_vk(
     let trusted_setup_file = Some(trusted_setup_file);
 
     let crs_mons = match trusted_setup_file {
-        Some(ref crs_file_str) => get_trusted_setup(crs_file_str),
+        Some(ref crs_file_str) => zkos_wrapper::get_trusted_setup(crs_file_str),
         None => Crs::<Bn256, CrsForMonomialForm>::crs_42(
             1 << L1_VERIFIER_DOMAIN_SIZE_LOG,
             &BellmanWorker::new(),
@@ -199,7 +139,7 @@ fn generate_vk(
         final_recursion_layer_verifier_vk().params
     };
 
-    let binary_commitment = create_binary_commitment(input_binary, &verifier_params);
+    let binary_commitment = zkos_wrapper::create_binary_commitment(input_binary, &verifier_params);
 
     let (_, _, _, risc_wrapper_vk, _, _, _) =
         get_risc_wrapper_setup(&boojum_worker, binary_commitment.clone());
@@ -212,7 +152,7 @@ fn generate_vk(
 
     let (_, snark_wrapper_vk) = get_snark_wrapper_setup(compression_vk.clone(), &crs_mons, &worker);
 
-    serialize_to_file(
+    zkos_wrapper::serialize_to_file(
         &snark_wrapper_vk,
         &Path::new(&output_dir.clone()).join("snark_vk_expected.json"),
     );
@@ -225,124 +165,3 @@ fn generate_vk(
     Ok(())
 }
 
-fn prove(
-    input: String,
-    input_binary: Option<String>,
-    output_dir: String,
-    trusted_setup_file: Option<String>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let crs_mons = match trusted_setup_file {
-        Some(ref crs_file_str) => get_trusted_setup(crs_file_str),
-        None => Crs::<Bn256, CrsForMonomialForm>::crs_42(
-            1 << L1_VERIFIER_DOMAIN_SIZE_LOG,
-            &BellmanWorker::new(),
-        ),
-    };
-
-    println!("=== Phase 1: Creating the Risc wrapper proof");
-
-    let worker = boojum::worker::Worker::new_with_num_threads(4);
-
-    let program_proof: zkos_wrapper::ProgramProof = deserialize_from_file(&input);
-    let binary_commitment = match input_binary {
-        Some(binary_path) => create_binary_commitment(binary_path, &program_proof.end_params),
-        None => BinaryCommitment::from_default_binary(),
-    };
-    let risc_wrapper_witness =
-        RiscWrapperWitness::from_full_proof(program_proof, &binary_commitment);
-
-    let (
-        finalization_hint,
-        setup_base,
-        setup,
-        risc_wrapper_vk,
-        setup_tree,
-        vars_hint,
-        witness_hints,
-    ) = get_risc_wrapper_setup(&worker, binary_commitment.clone());
-
-    let risc_wrapper_proof = prove_risc_wrapper(
-        risc_wrapper_witness,
-        &finalization_hint,
-        &setup_base,
-        &setup,
-        &risc_wrapper_vk,
-        &setup_tree,
-        &vars_hint,
-        &witness_hints,
-        &worker,
-        binary_commitment,
-    );
-    let is_valid = verify_risc_wrapper_proof(&risc_wrapper_proof, &risc_wrapper_vk);
-
-    assert!(is_valid);
-
-    serialize_to_file(
-        &risc_wrapper_proof,
-        &Path::new(&output_dir.clone()).join("risc_proof.json"),
-    );
-
-    println!("=== Phase 2: Creating compression proof");
-
-    let (
-        finalization_hint,
-        setup_base,
-        setup,
-        compression_vk,
-        setup_tree,
-        vars_hint,
-        witness_hints,
-    ) = get_compression_setup(risc_wrapper_vk.clone(), &worker);
-    let compression_proof = prove_compression(
-        risc_wrapper_proof,
-        risc_wrapper_vk,
-        &finalization_hint,
-        &setup_base,
-        &setup,
-        &compression_vk,
-        &setup_tree,
-        &vars_hint,
-        &witness_hints,
-        &worker,
-    );
-    let is_valid = verify_compression_proof(&compression_proof, &compression_vk);
-
-    assert!(is_valid);
-
-    serialize_to_file(
-        &compression_proof,
-        &Path::new(&output_dir.clone()).join("compression_proof.json"),
-    );
-
-    println!("=== Phase 3: Creating SNARK proof");
-
-    {
-        let worker = BellmanWorker::new_with_cpus(4);
-
-        let (snark_setup, snark_wrapper_vk) =
-            get_snark_wrapper_setup(compression_vk.clone(), &crs_mons, &worker);
-
-        let snark_wrapper_proof = prove_snark_wrapper(
-            compression_proof,
-            compression_vk,
-            &snark_setup,
-            &crs_mons,
-            &worker,
-        );
-
-        let is_valid = verify_snark_wrapper_proof(&snark_wrapper_proof, &snark_wrapper_vk);
-
-        assert!(is_valid);
-
-        serialize_to_file(
-            &snark_wrapper_proof,
-            &Path::new(&output_dir.clone()).join("snark_proof.json"),
-        );
-        serialize_to_file(
-            &snark_wrapper_vk,
-            &Path::new(&output_dir.clone()).join("snark_vk.json"),
-        );
-    }
-
-    Ok(())
-}
