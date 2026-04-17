f14f6e8d6545aee62533c1118c226f3b293e5d57
Oles Holembovskyy <oh@matterlabs.dev>
2026-04-17 10:16:26 +0000

    small change for CI

--- wrapper_generator/src/main.rs
+++ wrapper_generator/src/main.rs
@@ -63,12 +63,12 @@ fn format_rust_code(code: &str) -> Result<String, String> {
 struct Cli {
     #[arg(
         long,
-        default_value = "../wrapper/src/inner_verifiers/unified_reduced/imports"
+        default_value = "wrapper/src/inner_verifiers/unified_reduced/imports"
     )]
     output_dir: String,
     #[arg(
         long,
-        default_value = "../wrapper/src/inner_verifiers/blake_delegation/imports"
+        default_value = "wrapper/src/inner_verifiers/blake_delegation/imports"
     )]
     blake_output_dir: String,
 }
